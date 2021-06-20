fd0bdd77cc925261da5df437f1b9a63fd03adf3a
Aiden Harrington <aidenkharrington@gmail.com>
2021-06-20 16:15:20 -0700

    added discover screen

---
+++ lib/screens/discover_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/navigation_bar.dart';
+import '../widgets/placeholder_widget.dart';
+
+class DiscoverScreen extends StatefulWidget {
+  static const routeName = '/discover';
+  @override
+  _DiscoverScreenState createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Discover'),
+      ),
+      body: PlaceholderWidget(),
+      bottomNavigationBar: NavigationBar(1),
+    );
+  }
+}
