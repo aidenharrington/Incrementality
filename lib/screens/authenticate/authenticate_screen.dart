b9462d7e331d1919f975aa074d964fc40cd60d69
Aiden Harrington <aidenkharrington@gmail.com>
2021-06-27 17:27:26 -0700

    added authentication wrapper

---
+++ lib/screens/authenticate/authenticate_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthenticateScreen extends StatefulWidget {
+  @override
+  _AuthenticateScreenState createState() => _AuthenticateScreenState();
+}
+
+class _AuthenticateScreenState extends State<AuthenticateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Autenticate Screen'),
+    );
+  }
+}
