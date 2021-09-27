f39668be7359db21c208fa0409ba7d4acd0eb461
Aiden Harrington <aidenkharrington@gmail.com>
2021-09-27 07:53:22 -0700

    partial implementation of firebase auth mocking

---
+++ test/unit_tests/providers_tests/auth_provider_test.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:test/test.dart';
+import 'package:incrementality/providers/auth_provider.dart';
+import 'package:mockito/mockito.dart';
+
+class FirebaseAuthMock extends Mock implements FirebaseAuth {}
+
+void main() {
+  //Tests
+
+  //isAuthenticated
+  test(
+      'Given a firebase instance with no users When isAutenticated is called Then return false',
+      () async {
+    final mockAuth = FirebaseAuthMock();
+    when(mockAuth.signInWithEmailAndPassword(email: '', password: ''))
+        .thenAnswer((realInvocation) => Future.value(''));
+  });
+
+  //sign in good
+
+  //sign in no user
+
+  //sign in throws general error
+
+  //sign in not verified
+
+  //register good
+
+  //register email in use
+
+  //register password too weak
+
+  //register general error
+
+  //signout
+}
---
+++ test/widget_tests/screens_tests/sign_in_screen_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+import 'package:incrementality/screens/sign_in_screen.dart';
+
+void main() {
+  //Tests
+
+  //toggle registration with ui
+  // testToggleRegistrationMode('Toggle registration mode',
+  //     (WidgetTester tester) async {
+  //   await tester.pumpWidget(const Scaffold());
+
+  //   expect(find.byKey(key), matcher)
+  // });
+
+  //registration uncessful
+
+  //registration successful
+
+  //sign in unsuccessful
+
+  //sign in successful
+
+  //registration passwords don't match
+
+  //empty uname
+
+  //empty pname
+}
